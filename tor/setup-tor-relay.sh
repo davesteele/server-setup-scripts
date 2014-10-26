598e1d4b71e5ad1753f0c21850ebeb7ce5f92890
David Steele <dsteele@gmail.com>
2014-10-26 14:53:20 -0400

    setup-tor-relay.sh: Complete the configuration
    This should be a fully functional script for setting up a relay-
    only tor node.

--- tor/setup-tor-relay.sh
+++ tor/setup-tor-relay.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Note - this will ask for your distribution name (e.g. sid),
+# and traffic quota
+
 if grep -q torproject.org /etc/apt/sources.list ;
 then
   echo "Tor installation is already configured"
@@ -13,5 +16,30 @@ else
 fi
 
 apt-get -y update
-apt-get install deb.torproject.org-keyring
-apt-get install tor
+apt-get -y install deb.torproject.org-keyring
+apt-get -y install tor
+apt-get -y install pwgen
+apt-get -y install vim
+
+if grep -q "#SocksPolicy reject *" /etc/tor/torrc;
+then
+  sed -i 's/#SocksPolicy reject/SocksPolicy reject/' /etc/tor/torrc;
+  echo -n "Daily traffic quota (e.g. '15 GB'): ";
+  read quota;
+  echo >>/etc/tor/torrc;
+  echo "AccountingMax $quota" >>/etc/tor/torrc
+  echo "AccountingStart day 00:00" >>/etc/tor/torrc
+  export name=`pwgen -A0 8 -N 1`
+  echo "Nickname $name" >>/etc/tor/torrc
+  echo "I dub this relay $name (in /etc/tor/torrc)"
+  echo "ORPort 9001" >>/etc/tor/torrc
+  echo "ExitPolicy reject *:*" >> /etc/tor/torrc
+
+  echo "Check in /var/log/tor/log for the ORPort reachable test"
+  echo "https://www.torproject.org/docs/tor-doc-relay.html.en#check"
+else
+  echo "torrc already configured";
+fi
+
+service tor restart
+
